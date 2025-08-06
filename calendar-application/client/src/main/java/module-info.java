aa97c6e81f4896b5a1b55078b8fc60f023a85dec
Danek Jiri <jirkadanek@icloud.com>
2025-08-06 16:00:40 +0200

    update module-info

--- calendar-application/client/src/main/java/module-info.java
+++ calendar-application/client/src/main/java/module-info.java
@@ -5,7 +5,14 @@
 module cz.cuni.mff.danekji.calendar.client {
     requires cz.cuni.mff.danekji.calendar.core;
     requires org.apache.logging.log4j;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.logging;
 
-    exports cz.cuni.mff.danekji.calendar.client.cli.ui;
+    opens cz.cuni.mff.danekji.calendar.client.gui.controllers to javafx.fxml;
+
     exports cz.cuni.mff.danekji.calendar.client.cli;
+    exports cz.cuni.mff.danekji.calendar.client.cli.ui;
+    exports cz.cuni.mff.danekji.calendar.client.network;
+    exports cz.cuni.mff.danekji.calendar.client.gui;
 }
